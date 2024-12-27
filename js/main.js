bd6d3484d3c613571a6f60e6c589eca4a076e934
mitchmarns <sakurapizzaa@gmail.com>
2024-12-27 09:49:51 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("Hockey Simulation Loaded!");
+
+// Example function to test
+function initializeTeams() {
+    let teams = ["Rangers", "Devils", "Islanders", "Sabres"];
+    console.log("Teams:", teams);
+}
+
+// Call the function on load
+window.onload = function() {
+    initializeTeams();
+};
